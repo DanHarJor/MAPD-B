0119c1210ae196e744fa17104edd19c1cbd9fd44
danharjor <daniel.harley.jordan@outlook.com>
2022-07-07 11:09:20 +0200

    save

--- dask/notebooks/g6.py
+++ dask/notebooks/g6.py
@@ -1,6 +1,7 @@
 from tensorflow.keras import models, layers
 import tensorflow as tf
 import pickle
+path_weights='/weights/weights.pkl'
 def build_cnn():
     cnn = models.Sequential([
         layers.Conv2D(filters=30, kernel_size=(3,3), activation='relu', input_shape=(198,198,3)),
@@ -22,19 +23,19 @@ cnn = build_cnn()
 #make a pickle file on the worker with the initial weights
 def weights_init():
     weights = cnn.get_weights()
-    f_weights = open('/home/ubuntu/daskNotShared/weights.pkl', 'wb')
+    f_weights = open(path_weights, 'wb')
     pickle.dump(weights, f_weights)
     f_weights.close()
     
 # once the weights have been trained. This brings them from the workers to this machine for validation
 def weights_get():
-    f_weights = open('/home/ubuntu/daskNotShared/weights.pkl','rb')
+    f_weights = open(path_weights,'rb')
     weights = pickle.load(f_weights)
     f_weights.close()
     return weights
 #now we have the best weights we need to write them to each worker so they can re-train from the best weights
 def weights_set_best(weights_best):
-    f_weights = open('/home/ubuntu/daskNotShared/weights.pkl','wb')
+    f_weights = open(path_weights,'wb')
     pickle.dump(weights_best,f_weights)
     f_weights.close()
     del f_weights, weights_best
